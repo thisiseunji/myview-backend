7cc01a0e193ebde7c1b789115edd6eaa3ce809a9
kkookkss <pre0919@gmail.com>
2022-06-02 16:45:12 +0900

    FEAT: movie detile

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.http            import JsonResponse
+from rest_framework.views   import APIView
 
-# Create your views here.
+from movies.models          import Movie, MovieImage, ThumbnailImage, MovieActor, MovieGenre
+
+
+class MovieDetailView(APIView):
+    def get(self, request, movie_id):
+        # try:
+            movie           = Movie.objects.get(id=movie_id)
+            
+            data = {
+                    'title'           : movie.title,
+                    'description'     : movie.description,
+                    'release_date'    : movie.release_date,
+                    'country'         : movie.country.name,
+                    'category'        : movie.category.name,
+                    'genre'           : [movie_genre.genre.name for movie_genre in MovieGenre.objects.filter(movie_id=movie_id)],
+                    'actor'           : [{'id': movie_actor.actor.id,
+                                          'name':movie_actor.actor.name,
+                                          'role_name': movie_actor.role_name,
+                                          'role': movie_actor.role.name,
+                                          } for movie_actor in MovieActor.objects.filter(movie_id=movie_id)],  
+                    'thumbnail_image' : ThumbnailImage.objects.get(movie_id=movie_id).image.image_url,
+                    'movie_image'     : [image.image.image_url for image in MovieImage.objects.filter(movie_id=movie_id)]
+                    }
+            
+            return JsonResponse({'data':data})
--- myview/urls.py
+++ myview/urls.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('users/', include('users.urls')),
+    path('movies/', include('movies.urls')),
 ]
