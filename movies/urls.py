7234f0df801e1a246592221734295c80359ae546
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-06-02 18:49:04 +0900

    Add : movie title search data

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,9 @@
 from django.urls  import path
-from movies.views import MovieDetailView
+from movies.views import MovieDetailView, MovieTitleView
 
 urlpatterns = [
     path('<int:movie_id>', MovieDetailView.as_view()),
+    
+    #영화 제목 검색 데이터
+    path('titles', MovieTitleView.as_view())
 ]
