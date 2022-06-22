1f0af8ecb942b5ec8e7086b0ff3a1ddd50c4657a
kkookkss <pre0919@gmail.com>
2022-06-23 05:59:54 +0900

    Modify: gitignore, movie app views.py

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,5 @@
 from django.http             import JsonResponse
 from django.views            import View
-from django.db               import transaction
 from django.db.models        import Avg
 from rest_framework.views    import APIView
 from rest_framework.response import Response
