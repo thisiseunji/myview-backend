7d2cb98db3774396f866d9aafbc0c4fc2843626c
kkookkss <pre0919@gmail.com>
2022-07-03 14:14:28 +0900

    Refactor: actor detail api agency code modification

--- movies/views.py
+++ movies/views.py
@@ -102,7 +102,7 @@ class ActorDetailView(APIView):
                 'background_image' : AWS_S3_URL+random.choice(image_list),
                 'agency'           : actor.agency,
                 'starring_list'    : [{
-                    'movie_id'                  :movie_actor.movie.id,
+                    'movie_id'            :movie_actor.movie.id,
                     'title'               : movie_actor.movie.title,
                     'release'             : datetime.strftime(movie_actor.movie.release_date, '%Y'),
                     'thumbnail_image_url' : AWS_S3_URL+ThumbnailImage.objects.get(movie_id=movie_actor.movie.id).image.image_url,
